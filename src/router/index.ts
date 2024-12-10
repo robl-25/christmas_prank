30090c1f01a683d59499b20be2b9b0780320d773
Mariane Coelho <mariane_coelho@proton.me>
2024-12-10 15:44:47 -0700

    create project home

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // This can be re-write to component: () => import('../view/HomeView.vue')
+      // Which is a route level code-splitting,
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: HomeView,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="title">
+      <p>
+        Bem-vindas ao jogo de perguntas e respostas que te dá um presente de natal, caso você vença!
+      </p>
+    </div>
+  </main>
+</template>
+
+<style lang="css" scoped>
+.title {
+  font-size: xx-large;
+  color: burlywood;
+  font-family: Apple Chancery, cursive;
+  font-variant: small-caps;
+  text-align: center;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
