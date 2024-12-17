dec2dc0eb48b9a9a1cc3e04658d5bf78e168af2d
Mariane Coelho <marianerobl@protonmail.com>
2024-12-16 20:35:14 -0700

    change deploy script to use main branch

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ yarn run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git initgit add -A
+git init
+git add -A
 git commit -m 'deploy'
-git push -f git@github.com:robl-25/christmas_prank.git master:gh-pages
+git push -f git@github.com:robl-25/christmas_prank.git main:gh-pages
 cd -
